0a1c0565a8f66e21af21024db4fdb6b17da61f3d
Krzysiek Bielicki <krzysiek@skyman.pl>
2025-01-09 03:42:23 +0100

    another try

--- darwin/adv.go
+++ darwin/adv.go
@@ -5,20 +5,20 @@ import (
 	"github.com/raff/goble/xpc"
 )
 
-type adv struct {
+type Advertisement struct {
 	args xpc.Dict
 	ad   xpc.Dict
 }
 
-func (a *adv) LocalName() string {
+func (a *Advertisement) LocalName() string {
 	return a.ad.GetString("kCBAdvDataLocalName", a.args.GetString("kCBMsgArgName", ""))
 }
 
-func (a *adv) ManufacturerData() []byte {
+func (a *Advertisement) ManufacturerData() []byte {
 	return a.ad.GetBytes("kCBAdvDataManufacturerData", nil)
 }
 
-func (a *adv) ServiceData() []ble.ServiceData {
+func (a *Advertisement) ServiceData() []ble.ServiceData {
 	xSDs, ok := a.ad["kCBAdvDataServiceData"]
 	if !ok {
 		return nil
@@ -36,7 +36,7 @@ func (a *adv) ServiceData() []ble.ServiceData {
 	return sd
 }
 
-func (a *adv) Services() []ble.UUID {
+func (a *Advertisement) Services() []ble.UUID {
 	xUUIDs, ok := a.ad["kCBAdvDataServiceUUIDs"]
 	if !ok {
 		return nil
@@ -48,30 +48,30 @@ func (a *adv) Services() []ble.UUID {
 	return uuids
 }
 
-func (a *adv) OverflowService() []ble.UUID {
+func (a *Advertisement) OverflowService() []ble.UUID {
 	return nil // TODO
 }
 
-func (a *adv) TxPowerLevel() int {
+func (a *Advertisement) TxPowerLevel() int {
 	return a.ad.GetInt("kCBAdvDataTxPowerLevel", 0)
 }
 
-func (a *adv) SolicitedService() []ble.UUID {
+func (a *Advertisement) SolicitedService() []ble.UUID {
 	return nil // TODO
 }
 
-func (a *adv) Connectable() bool {
+func (a *Advertisement) Connectable() bool {
 	return a.ad.GetInt("kCBAdvDataIsConnectable", 0) > 0
 }
 
-func (a *adv) RSSI() int {
+func (a *Advertisement) RSSI() int {
 	return a.args.GetInt("kCBMsgArgRssi", 0)
 }
 
-func (a *adv) Address() ble.Addr {
+func (a *Advertisement) Address() ble.Addr {
 	return xpc.UUID(a.args.MustGetUUID("kCBMsgArgDeviceUUID"))
 }
 
-func (a *adv) Data() []byte {
+func (a *Advertisement) Data() []byte {
 	return nil
 }
